12b47aa7c406ae33d4bbd2f323e333f61d7892ef
mayurkadam34 <mayurkadam1209@gmail.com>
2024-12-11 17:47:28 +0530

    Fist commit

---
+++ ClassroomTeachers/src/main/java/com/tka/ClassroomTeachers/ClassroomTeachersApplication.java
@@ -0,0 +1,13 @@
+package com.tka.ClassroomTeachers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomTeachersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomTeachersApplication.class, args);
+	}
+
+}
---
+++ ClassroomTeachers/src/test/java/com/tka/ClassroomTeachers/ClassroomTeachersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tka.ClassroomTeachers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomTeachersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
